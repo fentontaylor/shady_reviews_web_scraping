4d9171ddb161fbad6ccaeedc1a821cd5ef014663
fentontaylor <fentontaylor@gmail.com>
2021-12-05 14:57:43 -0700

    Implement ReviewScraper.fetch_page_content

--- lib/review_scraper.py
+++ lib/review_scraper.py
@@ -1,3 +1,10 @@
+import requests
+
 class ReviewScraper:
     def __init__(self, base_url: str) -> None:
-        self.base_url = base_url
+        self.base_url = base_url[0:-1] if base_url[-1] == '/' else base_url
+
+    def fetch_page_content(self, uri: str) -> str:
+        url = self.base_url + (uri if uri[0] == '/' else '/' + uri)
+        page = requests.get(url)
+        return page.content
